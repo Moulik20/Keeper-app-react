fd373c9e43a9aad2808c961fd8823bd0cb092c59
Moulik Arora <you@example.com>
2021-08-02 16:15:42 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NoteIcon from '@material-ui/icons/Note';
+
+function Header() {
+  return (
+    <header>
+      <h1><NoteIcon/> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
